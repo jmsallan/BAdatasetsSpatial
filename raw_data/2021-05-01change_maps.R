4ab4848fc9c97cf5e4eb99c1b26740e27929f1fa
jmsallan <jose.maria.sallan@gmail.com>
2021-05-01 20:27:58 +0200

    rebuild world maps

--- raw_data/2021-05-01change_maps.R
+++ raw_data/2021-05-01change_maps.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 WorldMap1_110 %>% filter(NAME == "Spain") %>% select(ISO_A3, NAME, NAME_ES)
 
 sort(WorldMap1_110$NAME)
@@ -13,35 +13,48 @@ data("WorldMap1_110")
 
 WorldMap1_110 %>% filter(NAME %in% c("France", "Norway", "Kosovo")) %>% select(NAME, ISO_A2, ISO_A3)
 
-WorldMap1_110 <- WorldMap1_110 %>% 
+WorldMap1_110 <- WorldMap1_110 %>%
   mutate_if(is.factor, as.character)
 
-WorldMap1_110 <- WorldMap1_110 %>% 
+WorldMap1_110 <- WorldMap1_110 %>%
   mutate(ISO_A3 = replace(ISO_A3, NAME == "France", "FRA"))
-         
-WorldMap1_110 <- WorldMap1_110 %>% 
+
+WorldMap1_110 <- WorldMap1_110 %>%
   mutate(ISO_A3 = replace(ISO_A3, NAME == "Norway", "NOR"))
 
-WorldMap1_110 <- WorldMap1_110 %>% 
+WorldMap1_110 <- WorldMap1_110 %>%
   mutate(ISO_A3 = replace(ISO_A3, NAME == "Kosovo", "XKX"))
 
 WorldMap1_110 %>% filter(NAME %in% c("France", "Norway", "Kosovo")) %>% select(NAME, ISO_A2, ISO_A3)
+
+WorldMap1_110 <- WorldMap1_110 %>%
+  select(-featurecla, -GEOU_DIF, -BRK_GROUP, -LEVEL, -SU_DIF, -ADM0_A3_UN, -ADM0_A3_WB)
+
+WorldMap1_110 <- WorldMap1_110 %>%
+  select(-WIKIPEDIA)
+
+
+save(WorldMap1_110, file="data/WorldMap1_110.RData")
 
 data("WorldMap1_10")
 
 WorldMap1_10 %>% filter(NAME %in% c("France", "Norway", "Kosovo")) %>% select(NAME, ISO_A2, ISO_A3)
 
-WorldMap1_10 <- WorldMap1_10 %>% 
+WorldMap1_10 <- WorldMap1_10 %>%
   mutate_if(is.factor, as.character)
 
-WorldMap1_10 <- WorldMap1_10 %>% 
+WorldMap1_10 <- WorldMap1_10 %>%
   mutate(ISO_A3 = replace(ISO_A3, NAME == "France", "FRA"))
 
-WorldMap1_10 <- WorldMap1_10 %>% 
+WorldMap1_10 <- WorldMap1_10 %>%
   mutate(ISO_A3 = replace(ISO_A3, NAME == "Norway", "NOR"))
 
-WorldMap1_10 <- WorldMap1_10 %>% 
+WorldMap1_10 <- WorldMap1_10 %>%
   mutate(ISO_A3 = replace(ISO_A3, NAME == "Kosovo", "XKX"))
 
 WorldMap1_10 %>% filter(NAME %in% c("France", "Norway", "Kosovo")) %>% select(NAME, ISO_A2, ISO_A3)
 
+WorldMap1_10 <- WorldMap1_10 %>%
+  select(-featurecla, -GEOU_DIF, -LEVEL, -ADM0_A3_UN, -ADM0_A3_WB)
+
+save(WorldMap1_10, file = "data/WorldMap1_10.RData")
